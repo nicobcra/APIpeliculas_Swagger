69e874e0180546e166308e4fe79c9c6d779ade4c
nicobcra <nbecerran73@gmail.com>
2026-07-03 12:56:41 -0500

    API.NET - Capa Datos y Capa Modelos

---
+++ APIpeliculas/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APIpeliculas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIpeliculas.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //Aqui se ponen todas las entidades (Modelos)
+        public DbSet<Categoria>Categoria { get; set; }
+    }
+}
---
+++ APIpeliculas/Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIpeliculas.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Creacion: ")]
+        public DateTime FechaCreacion {  get; set; }
+
+    }
+}
